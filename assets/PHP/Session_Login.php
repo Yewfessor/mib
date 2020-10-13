1d9b603bfc012d1b80cbb774b7d9b239f732c368
gporawetku <gporawetku@github.com>
2020-10-14 00:56:27 +0700

    login

---
+++ assets/PHP/Session_Login.php
@@ -0,0 +1,26 @@
+<meta charset="utf-8">
+<?php
+session_start();
+if (isset($_POST['account_username'])) {
+    include("Connection.php");
+    $account_username = $_POST['account_username'];
+    $account_password = $_POST['account_password'];
+    $sql = "SELECT * FROM mib_acoount Where account_username = '" . $account_username . "' and account_password = '" . $account_password . "'";
+    $result = mysqli_query($connection, $sql);
+
+    if (mysqli_num_rows($result) == 1) {
+        $row = mysqli_fetch_array($result);
+        $_SESSION["account_id"] = $row["account_id"];
+        $_SESSION["account_username"] = $row["account_username"];
+        Header("Location: index.html");
+    } else {
+        session_destroy();
+        echo "<script>";
+        echo "alert(\" ชื่อผู้ใช้ หรือ รหัสผ่าน ไม่ถูกต้อง\");";
+        echo "window.history.back()";
+        echo "</script>";
+    }
+} else {
+    Header("Location: index.html"); //user & password incorrect back to login again
+}
+?>
