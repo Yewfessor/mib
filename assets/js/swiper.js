4df8fad89f7921317f5ed8adc8e5312f86171fab
Yewfessor <49652170+yiwfessor@users.noreply.github.com>
2020-10-15 17:44:21 +0700

    add Product slide

--- assets/js/swiper.js
+++ assets/js/swiper.js
@@ -26,3 +26,40 @@ var swiper3 = new Swiper('.swiper-container.box3', {
     grabCursor: true,
     loop: true,
 });
+
+var swiperProduct = new Swiper('.swiper-container.product-slide', {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+    loop: true,
+    mousewheel: true,
+    breakpoints: {
+        375: {
+            slidesPerView: 2,
+            spaceBetween: 10,
+        },
+        768: {
+            slidesPerView: 3,
+            spaceBetween: 40,
+        },
+        1024: {
+            slidesPerView: 3,
+            spaceBetween: 40,
+        },
+        1600: {
+            slidesPerView: 4,
+            spaceBetween: 50,
+        },
+        1900: {
+            slidesPerView: 5,
+            spaceBetween: 50,
+        },
+    },
+});
