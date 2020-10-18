d46e66c4d720631b994c3c5342b04823a3a9e67b
gporawetku <gporawetku@github.com>
2020-10-18 18:05:09 +0700

    upload rename finish

--- assets/php/upload.php
+++ assets/php/upload.php
@@ -6,15 +6,29 @@
 
 <body>
 	<?php
+
 	$date = date("Y-m-d");
 	$time = date("dmyHis");
-		if(move_uploaded_file($_FILES["fileUpload"]["tmp_name"],"../images/slide1/".$_FILES[$time]["name"])) // Upload/Copy
+	$path = "../images/slide1/";
+	$name = $_FILES["fileUpload"]["name"];
+	$tmp  = $_FILES["fileUpload"]["tmp_name"];
+
+	/*if(move_uploaded_file($_FILES["fileUpload"]["tmp_name"],"../images/slide1/".$_FILES["fileUpload"]["name"])) // Upload/Copy
 		{
-			echo "Copy/Upload Complete.";
 			include("connection.php");
-			$sql = "INSERT INTO mib_slide1 (slide1_pict_location,slide1_date) VALUES ('".$_FILES[$time]["name"]."','".$date."')";
+			$sql = "INSERT INTO mib_slide1 (slide1_pict_location,slide1_date) VALUES ('".$_FILES["fileUpload"]["name"]."','".$date."')";
 			$result = mysqli_query($connection,$sql);
 			Header("Location: ../../admin.php");
-		}
+		}*/
+	if (strlen($name)) {
+		list($txt, $ext) = explode(".", $name);
+		$new_file_name = $time . "." . $ext;
+		move_uploaded_file($tmp, $path . $new_file_name);
+		include("connection.php");
+		$sql = "INSERT INTO mib_slide1 (slide1_pict_location,slide1_date) 
+		VALUES ('" . $new_file_name . "','" . $date . "')";
+		$result = mysqli_query($connection, $sql);
+		Header("Location: ../../admin.php");
+	}
 	?>
 </body>
