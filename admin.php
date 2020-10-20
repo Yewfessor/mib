e9456093c18d3ee54b3a5a108563b40db7a432c9
gporawetku <gporawetku@github.com>
2020-10-20 14:12:45 +0700

    re admin

--- admin.php
+++ admin.php
@@ -2,37 +2,15 @@
 
 <head>
 	<title></title>
-	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
 </head>
 
 <body>
-	<div id="details" class="basic-2">
-		<div class="container">
-			<div class="row">
 
-				<div class="col-lg-6">
-					<div class="text-contain">
-						<a href="" class="solid popup">edit</a>
-					</div>
-				</div>
-			</div>
-		</div>
-	</div>
-
-	<div id="details-lightbox-1" class="lightbox-basic zoom mfp-hide">
-		<div class="row">
-			<button title="Close (Esc)" type="button" class="mfp-close x-button">x</button>
-			<div class="col-lg-6">
-				<form action="assets/php/upload.php" method="post" enctype="multipart/form-data" name="upload_slide1">
-					<input type="file" name="fileUpload">
-					<input type="submit" name="Submit" value="Upload">
-				</form>
-			</div>
-		</div>
-	</div>
-
-
-
+<div>
+	<form action="assets/php/upload.php" method="post" enctype="multipart/form-data" name="upload_slide1">
+		<input type="file" name="fileUpload">
+		<input type="submit" name="Submit" value="Upload">
+	</form>
 
 	<?php
 	include("assets/php/connection.php");
@@ -40,13 +18,23 @@
 	$result = mysqli_query($connection, $sql);
 	while ($row = mysqli_fetch_array($result)) {
 	?>
+
 		<img width="200" src="assets/images/slide1/<?php echo $row["slide1_pict_location"]; ?>">
 		<div><?php echo $row["slide1_date"]; ?></div>
-		<!--div><a href="delete.php">Delete</a></div>-->
+		<a href="" class="solid popup">Edit</a> <a href="" class="solid popup">Delete</a><br>
+
 	<?php
+
 	}
 	mysqli_close($connection);
+
 	?>
+</div>
+
+<div>
+
+</div>
+
 </body>
 
 </html>
