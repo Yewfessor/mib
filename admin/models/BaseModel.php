c10bad9cdb7e7e32ee785e9a5be25bb82eeacc26
gporawetku <gporawetku@github.com>
2020-11-20 01:53:21 +0700

    host

--- admin/models/BaseModel.php
+++ admin/models/BaseModel.php
@@ -1,5 +1,5 @@
 <?php
-   $server     = "localhost";
+   	$server     = "localhost";
 	$username   = "root";
 	$password   = "";
 	$database   = "mib";
@@ -8,6 +8,12 @@
 	$username   = "id15331195_mibthailand";
 	$password   = "j^S)O|5-@>QuJ^1l";
 	$database   = "id15331195_mib"; */
+
+	/*$server  	= "148.72.232.182:3306";
+	$username   = "ph17385860351";
+	$password   = "Mib2017!";
+	$database   = "ph17385860351_mib";*/
+
 	
 	$connection = mysqli_connect($server,$username,$password,$database) or die("Error: ".mysqli_error($connection));
 	mysqli_query($connection,"SET NAMES 'utf8'");
