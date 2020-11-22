57ca61b6bb7be7936e1fcf07d719b7bffbb4d941
gporawetku <gporawetku@github.com>
2020-11-22 15:53:19 +0700

    sql+css

--- admin/index.php
+++ admin/index.php
@@ -12,7 +12,11 @@ date_default_timezone_set("Asia/Bangkok");
 	<title>M.I.B ADMIN</title>
 	<link rel="stylesheet" href="../assets/css/admin.css">
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css" integrity="sha512-+4zCK9k+qNFUR5X+cKL9EIR+ZOhtIloNl9GIKS57V1MyNsYpYcUrUeQc9vNfzsWfV28IaLL3i96P9sdNyeRssA==" crossorigin="anonymous" />
-
+	<!--bootstrap -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+	<!-- ckeditor-->
+	<script src="//cdn.ckeditor.com/4.6.2/standard/ckeditor.js"></script>
+	<!-- Slide-->
 	<script>
 		const openSlideMenu = () => {
 			document.getElementById('menu').style.width = '250px';
--- admin/views/product.php
+++ admin/views/product.php
@@ -9,81 +9,97 @@ $path_image         = "../assets/images/product/";
 ?>
 
 <!--Upload Detail Product-->
+<div class="container">
+    <div class="row">
+        <div class="col-md-12">
+            <br><br><br>
+            <form class="form-horizontal" action="<?php echo $path_modelinput; ?>" id="productinput" enctype="multipart/form-data" name="productinput" method="post">
 
-<form action="<?php echo $path_modelinput; ?>" id="productinput" enctype="multipart/form-data" name="productinput" method="post">
-    <div class="form-container">
-        <table class="product-form-table">
-            <tr>
-                <td>Image</td>
-                <td><input type="file" name="product_image" id="product_image" required></td>
-            </tr>
-            <tr>
-                <td>Name</td>
-                <td><input type="text" name="product_name_en" id="product_name_en" value="" required></td>
-            </tr>
-            <tr>
-                <td>Description</td>
-                <td><input type="text" name="product_description_en" id="product_description_en" value=""></td>
-            </tr>
-            <tr>
-                <td>Detail</td>
-                <td><textarea name="product_detail_en" id="product_detail_en"></textarea></td>
-            </tr>
-            <tr>
-                <td>Price</td>
-                <td><input type="text" name="product_price" id="product_price" pattern="[0-9]{1,}" title="กรอกตัวเลขเท่านั้น" value="" required></td>
-            </tr>
-            <tr>
-                <td>Category</td>
-                <td>
-                    <select name="product_type_id" id="product_type_id" required>
-                        <option center value="">-Select-</option>
-                        <?php
-                        include $path_basemodel;
-                        $sql = "SELECT * FROM tb_product_type";
-                        $result = mysqli_query($connection, $sql);
-                        while ($row = mysqli_fetch_array($result)) {
-                        ?>
-                            <option value="<?php echo $row["product_type_id"]; ?>"><?php echo $row["product_type_name"]; ?></option>
-                        <?php
-                        }
-                        mysqli_close($connection);
-                        ?>
-                    </select>
-                </td>
-            </tr>
-            <tr>
-                <td>Type</td>
-                <td>
-                    <select name="product_line_up_id" id="product_line_up_id" required>
-                        <option center value="">-Select-</option>
-                        <?php
-                        include $path_basemodel;
-                        $sql = "SELECT * FROM tb_product_line_up 
+                <div class="form-group">
+                    <div class="col-sm-2" align="right">ชื่อสินค้า : </div>
+                    <div class="col-sm-8" align="left">
+                        <input class="form-control" type="text" name="product_name_en" id="product_name_en" value="" required>
+                    </div>
+                </div>
+
+                <div class="form-group">
+                    <div class="col-sm-2" align="right">คำอธิบาย : </div>
+                    <div class="col-sm-8" align="left">
+                        <input class="form-control" type="text" name="product_description_en" id="product_description_en" value="">
+                    </div>
+                </div>
+
+                <div class="form-group">
+                    <div class="col-sm-2" align="right">ประเภทสินค้า :</div>
+                    <div class="col-sm-3" align="left">
+                        <select class="form-control" name="product_type_id" id="product_type_id" required>
+                            <option center value="">-Select-</option>
+                            <?php
+                            include $path_basemodel;
+                            $sql = "SELECT * FROM tb_product_type";
+                            $result = mysqli_query($connection, $sql);
+                            while ($row = mysqli_fetch_array($result)) {
+                            ?>
+                                <option value="<?php echo $row["product_type_id"]; ?>"><?php echo $row["product_type_name"]; ?></option>
+                            <?php
+                            }
+                            mysqli_close($connection);
+                            ?>
+                        </select>
+                    </div>
+                    <div class="col-sm-4" align="left">
+                        <select class="form-control" name="product_line_up_id" id="product_line_up_id" required>
+                            <option center value="">-Select-</option>
+                            <?php
+                            include $path_basemodel;
+                            $sql = "SELECT * FROM tb_product_line_up 
                                 LEFT JOIN tb_product_type
-                                ON tb_product_line_up.product_type_id = tb_product_type.product_type_id
-                        ";
-                        $result = mysqli_query($connection, $sql);
-                        while ($row = mysqli_fetch_array($result)) {
-                        ?>
-                            <option value="<?php echo $row["product_line_up_id"]; ?>"><?php echo $row["product_type_name"] . " | " . $row["product_line_up_name"]; ?></option>
-                        <?php
-                        }
-                        mysqli_close($connection);
-                        ?>
-                    </select>
-                </td>
-            </tr>
-            <tr>
-                <td>&nbsp;</td>
-            </tr>
-            <tr>
-                <td colspan="2" class="submit-btn"><input type="submit" name="productsubmit" class="save-btn" value="Save"></td>
-            </tr>
-        </table>
-    </div>
-</form>
+                                ON tb_product_line_up.product_type_id = tb_product_type.product_type_id";
+                            $result = mysqli_query($connection, $sql);
+                            while ($row = mysqli_fetch_array($result)) {
+                            ?>
+                                <option value="<?php echo $row["product_line_up_id"]; ?>"><?php echo $row["product_type_name"] . " | " . $row["product_line_up_name"]; ?></option>
+                            <?php
+                            }
+                            mysqli_close($connection);
+                            ?>
+                        </select>
+                    </div>
+                </div>
 
+                <div class="form-group">
+                    <div class="col-sm-2" align="right">รายละเอียด :</div>
+                    <div class="col-sm-8" align="left">
+                        <textarea class="ckeditor" cols="69" rows="5" name="product_detail_en" id="product_detail_en"></textarea>
+                    </div>
+                </div>
+
+                <div class="form-group">
+                    <div class="col-sm-2" align="right">ราคา : </div>
+                    <div class="col-sm-2" align="left">
+                        <input class="form-control" type="text" name="product_price" id="product_price" pattern="[0-9]{1,}" title="กรอกตัวเลขเท่านั้น" value="" required>
+                    </div>
+                    <div>บาท</div>
+
+                </div>
+
+                <div class="form-group">
+                    <div class="col-sm-2" align="right">ไฟล์ประกอบ</div>
+                    <div class="col-sm-7" align="left">
+                        <input type="file" name="product_image" id="product_image" required></td>
+                    </div>
+                </div>
+
+                <div class="form-group">
+                    <div class="col-sm-2"> </div>
+                    <div class="col-sm-8" align="right">
+                        <input class="btn btn-primary" type="submit" name="productsubmit" value="Save">
+                    </div>
+                </div>
+            </form>
+        </div>
+    </div>
+</div>
 <br>
 
 <form name="productsearch" method="get" action="">
@@ -98,104 +114,103 @@ $path_image         = "../assets/images/product/";
     </table>
 </form>
 
-<?php
-if (isset($_GET["productall"]) != "") {
-    $i = 1;
-    include $path_basemodel;
-    $sql = "SELECT * FROM tb_product 
-            LEFT JOIN tb_product_type
-            ON tb_product.product_type_id = tb_product_type.product_type_id
-            LEFT JOIN tb_product_line_up
-            ON tb_product.product_line_up_id = tb_product_line_up.product_line_up_id
-            WHERE product_id = '" . $_GET["productall"] . "'";
-    $result = mysqli_query($connection, $sql);
-?>
-    <div style=" width:1000px; height:425px; overflow: auto;">
-        <form>
-            <table border="0" cellpadding="3" cellspacing="1" bgcolor="#CCCCCC">
-                <tr align="center" bgcolor="#FFFFFF">
-                    <td>Id</td>
-                    <td>Image </td>
-                    <td>Name</td>
-                    <td>Description</td>
-                    <td>Detail</td>
-                    <td>Price</td>
-                    <td>Category</td>
-                    <td>Type</td>
-                    <td>Edit</td>
-                    <td>Delete</td>
-                </tr>
-                <?php
-                while ($row = mysqli_fetch_array($result)) {
-                ?>
+<div class="container">
+    <div class="row">
+        <div style=" width:100%; height:425px; overflow: auto;" class="col-md-12">
+            <br><br>
+            <?php
+            if (isset($_GET["productall"]) != "") {
+                $i = 1;
+                include $path_basemodel;
+                $sql = "SELECT * FROM tb_product 
+                            LEFT JOIN tb_product_type
+                            ON tb_product.product_type_id = tb_product_type.product_type_id
+                            LEFT JOIN tb_product_line_up
+                            ON tb_product.product_line_up_id = tb_product_line_up.product_line_up_id
+                            WHERE product_id = '" . $_GET["productall"] . "'";
+                $result = mysqli_query($connection, $sql);
+            ?>
+                <table border="0" cellpadding="3" cellspacing="1" bgcolor="#CCCCCC">
                     <tr align="center" bgcolor="#FFFFFF">
-                        <td><?php echo $row["product_id"] ?></td>
-                        <td><img height="100" src="<?php echo $path_image . $row["product_image"]; ?>"></td>
-                        <td><?php echo $row["product_name_en"]; ?></td>
-                        <td><?php echo $row["product_description_en"]; ?></td>
-                        <td><?php echo $row["product_detail_en"]; ?></td>
-                        <td><?php echo number_format($row["product_price"]); ?> บาท</td>
-                        <td><?php echo $row["product_type_name"]; ?></td>
-                        <td><?php echo $row["product_line_up_name"]; ?></td>
-                        <td><a href="<?php echo $path_modeledit . "?edit_id=" . $row["product_id"]; ?>">แก้ไข </a></td>
-                        <td><a href="<?php echo $path_modeldelete . "?delete_id=" . $row["product_id"] . "&delete_img=" . $row["product_image"]; ?>" onclick="return confirm('ต้องการลบข้อมูลหรือไม่')">ลบ</a></td>
+                        <td>Id</td>
+                        <td>Image </td>
+                        <td>Name</td>
+                        <td>Description</td>
+                        <td>Detail</td>
+                        <td>Price</td>
+                        <td>Category</td>
+                        <td>Type</td>
+                        <td>Edit</td>
+                        <td>Delete</td>
                     </tr>
+                    <?php
+                    while ($row = mysqli_fetch_array($result)) {
+                    ?>
+                        <tr align="center" bgcolor="#FFFFFF">
+                            <td><?php echo $row["product_id"] ?></td>
+                            <td><img height="100" src="<?php echo $path_image . $row["product_image"]; ?>"></td>
+                            <td><?php echo $row["product_name_en"]; ?></td>
+                            <td><?php echo $row["product_description_en"]; ?></td>
+                            <td><?php echo $row["product_detail_en"]; ?></td>
+                            <td><?php echo number_format($row["product_price"]); ?> บาท</td>
+                            <td><?php echo $row["product_type_name"]; ?></td>
+                            <td><?php echo $row["product_line_up_name"]; ?></td>
+                            <td><a href="<?php echo $path_modeledit . "?edit_id=" . $row["product_id"]; ?>">แก้ไข </a></td>
+                            <td><a href="<?php echo $path_modeldelete . "?delete_id=" . $row["product_id"] . "&delete_img=" . $row["product_image"]; ?>" onclick="return confirm('ต้องการลบข้อมูลหรือไม่')">ลบ</a></td>
+                        </tr>
+                    <?php
+                        $i++;
+                    }
+                    mysqli_close($connection);
+                    ?>
+                </table>
+            <?php
+
+            } else if (isset($_GET["productall"]) == "") {
+                $i = 1;
+                include $path_basemodel;
+                $sql = "SELECT * FROM tb_product 
+                            LEFT JOIN tb_product_type
+                            ON tb_product.product_type_id = tb_product_type.product_type_id
+                            LEFT JOIN tb_product_line_up
+                            ON tb_product.product_line_up_id = tb_product_line_up.product_line_up_id";
+                $result = mysqli_query($connection, $sql);
+            ?>
+                <table border="0" cellpadding="3" cellspacing="1" bgcolor="#CCCCCC">
+                    <tr align="center" bgcolor="#FFFFFF">
+                        <td>Id</td>
+                        <td>Image </td>
+                        <td>Name</td>
+                        <td>Description</td>
+                        <td>Detail</td>
+                        <td>Price</td>
+                        <td>Category</td>
+                        <td>Type</td>
+                        <td>Edit</td>
+                        <td>Delete</td>
+                    </tr>
+                    <?php
+                    while ($row = mysqli_fetch_array($result)) {
+                    ?>
+                        <tr align="center" bgcolor="#FFFFFF">
+                            <td><?php echo $row["product_id"] ?></td>
+                            <td><img height="100" src="<?php echo $path_image . $row["product_image"]; ?>"></td>
+                            <td><?php echo $row["product_name_en"]; ?></td>
+                            <td><?php echo $row["product_description_en"]; ?></td>
+                            <td><?php echo $row["product_detail_en"]; ?></td>
+                            <td><?php echo number_format($row["product_price"]); ?> บาท</td>
+                            <td><?php echo $row["product_type_name"]; ?></td>
+                            <td><?php echo $row["product_line_up_name"]; ?></td>
+                            <td><a href="<?php echo $path_modeledit . "?edit_id=" . $row["product_id"]; ?>">แก้ไข </a></td>
+                            <td><a href="<?php echo $path_modeldelete . "?delete_id=" . $row["product_id"] . "&delete_img=" . $row["product_image"]; ?>" onclick="return confirm('ต้องการลบข้อมูลหรือไม่')"><i class="fa fa-trash" aria-hidden="true"></i></a></td>
+                        </tr>
                 <?php
-                    $i++;
+                        $i++;
+                    }
                 }
                 mysqli_close($connection);
                 ?>
-            </table>
-        </form>
+                </table>
+        </div>
     </div>
-<?php
-
-} else if (isset($_GET["productall"]) == "") {
-    $i = 1;
-    include $path_basemodel;
-    $sql = "SELECT * FROM tb_product 
-            LEFT JOIN tb_product_type
-            ON tb_product.product_type_id = tb_product_type.product_type_id
-            LEFT JOIN tb_product_line_up
-            ON tb_product.product_line_up_id = tb_product_line_up.product_line_up_id";
-    $result = mysqli_query($connection, $sql);
-?>
-    <div style=" width:1000px; height:425px; overflow: auto;">
-        <form>
-            <table border="0" cellpadding="3" cellspacing="1" bgcolor="#CCCCCC">
-                <tr align="center" bgcolor="#FFFFFF">
-                    <td>Id</td>
-                    <td>Image </td>
-                    <td>Name</td>
-                    <td>Description</td>
-                    <td>Detail</td>
-                    <td>Price</td>
-                    <td>Category</td>
-                    <td>Type</td>
-                    <td>Edit</td>
-                    <td>Delete</td>
-                </tr>
-                <?php
-                while ($row = mysqli_fetch_array($result)) {
-                ?>
-                    <tr align="center" bgcolor="#FFFFFF">
-                        <td><?php echo $row["product_id"] ?></td>
-                        <td><img height="100" src="<?php echo $path_image . $row["product_image"]; ?>"></td>
-                        <td><?php echo $row["product_name_en"]; ?></td>
-                        <td><?php echo $row["product_description_en"]; ?></td>
-                        <td><?php echo $row["product_detail_en"]; ?></td>
-                        <td><?php echo number_format($row["product_price"]); ?> บาท</td>
-                        <td><?php echo $row["product_type_name"]; ?></td>
-                        <td><?php echo $row["product_line_up_name"]; ?></td>
-                        <td><a href="<?php echo $path_modeledit . "?edit_id=" . $row["product_id"]; ?>">แก้ไข </a></td>
-                        <td><a href="<?php echo $path_modeldelete . "?delete_id=" . $row["product_id"] . "&delete_img=" . $row["product_image"]; ?>" onclick="return confirm('ต้องการลบข้อมูลหรือไม่')"><i class="fa fa-trash" aria-hidden="true"></i></a></td>
-                    </tr>
-            <?php
-                    $i++;
-                }
-            }
-            mysqli_close($connection);
-            ?>
-            </table>
-        </form>
-    </div>
+</div>
